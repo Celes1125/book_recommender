7f2de3ae907959ca751fb64cab8b96463c1d1a04
cc <colauttiadad@gmail.com>
2025-08-18 20:19:50 +0200

    env eliminated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,2 @@
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCf2cqJR3MSIN02Jv95oGn-iI1omhFW3hs",
-    authDomain: "book-recommender-fe795.firebaseapp.com",
-    projectId: "book-recommender-fe795",
-    storageBucket: "book-recommender-fe795.firebasestorage.app",
-    messagingSenderId: "69561009801",
-    appId: "1:69561009801:web:e042240fdba905a0afa858"
-  },
-  apiUrl: 'http://localhost:5000'
-};
 
 
