8c44c4b04b8b52656924293541af353f81a39ba1
cc <colauttiadad@gmail.com>
2026-05-15 11:57:12 +0200

    feat: migrate to pnpm and update dependencies, resolve security vulnerabilities

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,2 +1,12 @@
-
-
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'development_key',
+    authDomain: 'development_domain',
+    projectId: 'development_id',
+    storageBucket: 'development_bucket',
+    messagingSenderId: 'development_messaging',
+    appId: 'development_app'
+  },
+  apiUrl: 'http://localhost:5000'
+};
